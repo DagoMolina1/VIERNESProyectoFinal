1221967153e66bf83287f1e58c9865230c062c02
DagoMolia1 <julianmolia902@gmail.com>
2022-11-21 22:08:23 -0500

    Falta pintar las habitaciones en el front

---
+++ src/app/hotelservices/hotelservices.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hotelservices',
+  templateUrl: './hotelservices.component.html',
+  styleUrls: ['./hotelservices.component.css']
+})
+export class HotelservicesComponent {
+
+}
